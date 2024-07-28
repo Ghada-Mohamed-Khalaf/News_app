04ec9be739ca33c9cc6d13a7731901cd3f4308ee
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-07-29 00:06:11 +0300

    add  CategoryListView

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/widgets/category_card.dart';
+import 'package:news_app/widgets/categorys_list_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,15 +26,7 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.transparent,
       ),
-      body: SizedBox(
-        height:100 ,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal ,
-          itemCount: 10,
-            itemBuilder: (context,index){
-             return CategoryCard();
-            }),
-      ),
+      body:  CategoryListView(),
     );
   }
 }
