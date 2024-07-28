834a3ad75e7a227af9ed07cd99a216fd68005969
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-07-28 15:25:26 +0300

    create  appbar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,7 +5,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(appBar: AppBar(
+      title: const Text("News Cloud"),
+    ),
 
     );
   }
