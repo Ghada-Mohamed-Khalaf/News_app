75a2592c79987ba4e6c680b76f117c6069eadd20
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-07-30 23:26:17 +0300

    add list in category listView

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/material.dart';
+
 class NewsTile extends StatelessWidget {
   const NewsTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return
+    return Column(
+     mainAxisSize: MainAxisSize.min,
+      children: [
+      // ClipRRect(
+      //   borderRadius: BorderRadius.circular(6),
+      //   child:  Image.network(src),
+      // )
+
+      ],
+
+    );
   }
 }
