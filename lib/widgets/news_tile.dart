e51fce9cfeef815534987fd9a629b70f5cb6b646
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-07-31 14:47:07 +0300

    add sizedBox

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -6,15 +6,32 @@ class NewsTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-     mainAxisSize: MainAxisSize.min,
+      mainAxisSize: MainAxisSize.min,
       children: [
-      ClipRRect(
-        borderRadius: BorderRadius.circular(6),
-        child:  Image.network("https://english.ahram.org.eg/UI/Front/MultimediaInner.aspx?NewsContentID=502316&newsportalname=Multimedia"),
-      )
-
-      ],
-
+        ClipRRect(
+          borderRadius: BorderRadius.circular(6),
+          child: Image.network(
+            "https://english.ahram.org.eg/UI/Front/MultimediaInner.aspx?NewsContentID=502316&newsportalname=Multimedia",
+            height: 200,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+      //   Text(
+      //     "large Title should be places in this place large",
+      //     maxLines: 2,
+      //     overflow: TextOverflow.ellipsis,
+      //     style: TextStyle(
+      //         color: Colors.black87, fontSize: 20, fontWeight: FontWeight.w300),
+      //   ),
+      //   SizedBox(
+      //     height: 8,
+      //   ),
+      //   const Text("and here is the description of the news you can place",maxLines: 2,style: TextStyle(color:Colors.grey,fontSize:14 ),)
+       ],
     );
   }
 }
