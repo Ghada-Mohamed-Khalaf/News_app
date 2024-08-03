c64c4d1abe955ae28fa935df0ffccbbe38ba97ee
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-04 00:02:33 +0300

    add list of articles

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/services/news_service.dart';
 import 'package:news_app/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+  NewService(Dio()).getNews;
 }
 final dio = Dio();
 
-void getHttp() async {
-  final response = await dio.get('https://dart.dev');
-  print(response);
-}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
