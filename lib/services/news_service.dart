2c5e1098d7d4b306d51579277e0376ffe084e5be
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-03 19:33:19 +0300

    add async and await

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,20 +1,21 @@
 
+
+import 'dart:js_interop_unsafe';
+
 import 'package:dio/dio.dart';
+import 'package:news_app/main.dart';
 
-class NewsService {
+class NewService{
+
   final Dio dio;
+  NewService(this.dio);
 
-  NewsService(this.dio);
+}
 
-  void getGeneralNews() async {
-    final response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=3c88955c487e4d9db668f011dd85e737&category');
-    print(response);
-  }
 
-  void getSportNews() async {
-    final response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=3c88955c487e4d9db668f011dd85e737&category');
-    print(response);
-  }
+getNews() async{
+  Response response = await dio.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=3c88955c487e4d9db668f011dd85e737&category');
+
+  response.data;
+
 }
