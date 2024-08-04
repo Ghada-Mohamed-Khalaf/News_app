da36bb80bb03b8c2592e80cc7dc8af8ffb2a159d
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-04 16:06:35 +0300

    create articlesModel

---
+++ lib/models/article_model.dart
@@ -0,0 +1,7 @@
+class ArticleModel{
+final  String image;
+final String title;
+final  String subTile;
+ArticleModel({required this.title,required this.image,required this.subTile});
+
+}
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -17,7 +17,11 @@ getNews() async{
   Response response = await dio.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=3c88955c487e4d9db668f011dd85e737&category');
 
   Map<String,dynamic> jsonData = response.data;
-  print(jsonData["totalResults"]);
+  List<dynamic>articles =jsonData['articles'] ;
+  print(jsonData['articles ']);
+  for (var article in articles ) {
+    print(article['author']);
+  }
 
 
 }
